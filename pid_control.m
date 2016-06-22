b54a5dce0dc0de18699e98dc8773c2ecaa8690bd
cosine <urillx@gmail.com>
2016-06-22 11:38:55 -0400

    clean up

--- pid_control.m
+++ pid_control.m
@@ -1,6 +1,4 @@
 function [ dx ] = pid_control( x, Kp, Ki, Kd )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
 persistent i_term;
 if (isempty(i_term))
     i_term = x(1,:) * 0; 
--- rungeKutta4.m
+++
@@ -1,8 +0,0 @@
-function X_next = rungeKutta4(f,X_i,U_i,t_i,dt)
-k1 = f(X_i, U_i, t_i);
-k2 = f(X_i + k1*dt/2, U_i, t_i + dt/2);
-k3 = f(X_i + k2*dt/2, U_i, t_i + dt/2);
-k4 = f(X_i + k3*dt/2, U_i, t_i + dt);
-X_next = X_i + (dt/6)*(k1 + 2*k2 + 2*k3 + k4);
-end
-
